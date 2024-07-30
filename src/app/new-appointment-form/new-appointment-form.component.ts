6ef0e4f84dd86f65563074aaabb450ccaab3a83f
Kam <erkamyaman35@gmail.com>
2024-07-30 22:11:24 +0300

    ngModel-for-the-values

--- src/app/new-appointment-form/new-appointment-form.component.ts
+++ src/app/new-appointment-form/new-appointment-form.component.ts
@@ -2,14 +2,23 @@ import { Component } from '@angular/core';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { CardModule } from 'primeng/card';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-appointment-form',
   standalone: true,
-  imports: [FloatLabelModule, CardModule],
+  imports: [FloatLabelModule, CardModule, FormsModule],
   templateUrl: './new-appointment-form.component.html',
   styleUrl: './new-appointment-form.component.css'
 })
 export class NewAppointmentFormComponent {
+  customername = '';
+  title = '';
+  handleForm(event: any) {
+    event.preventDefault()
+    console.log(this.customername, this.title)
+    this.customername = '';
+    this.title = ''
 
+  }
 }
