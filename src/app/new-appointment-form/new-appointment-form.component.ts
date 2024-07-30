4e543cc3f62d36b4a55e2ba8e464694b39d7ad0d
Kam <erkamyaman35@gmail.com>
2024-07-30 22:37:43 +0300

    ngCalender-done

--- src/app/new-appointment-form/new-appointment-form.component.ts
+++ src/app/new-appointment-form/new-appointment-form.component.ts
@@ -1,22 +1,38 @@
 import { Component } from '@angular/core';
 import { FloatLabelModule } from 'primeng/floatlabel';
-import { CardModule } from 'primeng/card';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { CalendarModule } from 'primeng/calendar';
+
 
 @Component({
   selector: 'app-new-appointment-form',
   standalone: true,
-  imports: [FloatLabelModule, CardModule, FormsModule],
+  imports: [
+    FloatLabelModule,
+    InputTextModule,
+    ButtonModule,
+    CardModule,
+    FormsModule,
+    CalendarModule,
+    CommonModule
+  ],
+
   templateUrl: './new-appointment-form.component.html',
   styleUrl: './new-appointment-form.component.css'
 })
 export class NewAppointmentFormComponent {
   customername = '';
   title = '';
+  date = '';
+  time = '';
   handleForm(event: any) {
     event.preventDefault()
-    console.log(this.customername, this.title)
+    console.log(this.customername, this.title, this.date, this.time)
     this.customername = '';
     this.title = ''
 
