52e7d382ca9a8b2a67f7b628ccfd27fce77a9e80
Kam <erkamyaman35@gmail.com>
2024-07-30 08:07:34 +0300

    Angular-Itterating-Arays

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Appointment } from '../shared/models/appoinment';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Angular-Appointment-Application';
+  appointments = [
+    new Appointment("Erkam Yaman", new Date(), "Hair"),
+    new Appointment("Erkam Yaman", new Date(), "Nail"),
+    new Appointment("Erkam Yaman", new Date(), "Shave")]
 }
---
+++ src/shared/models/appoinment.ts
@@ -0,0 +1,6 @@
+export class Appointment {
+    constructor(public customerName: string, public appointmentDate: Date, public appointmentTitle: string) {
+
+    }
+
+}
