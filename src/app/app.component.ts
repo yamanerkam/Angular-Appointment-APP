3f93fa5278667a7c1c3de600cd9a745aad52bb26
Kam <erkamyaman35@gmail.com>
2024-07-30 18:33:56 +0300

    app-component-cleaned

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { Appointment } from '../shared/models/appoinment';
-import { ButtonModule } from 'primeng/button';
-import { CommonModule } from '@angular/common';
-import { TableModule } from 'primeng/table';
-import { InputTextModule } from 'primeng/inputtext';
-import { CardModule } from 'primeng/card';
-import { FloatLabelModule } from 'primeng/floatlabel';
-import { CalendarModule } from 'primeng/calendar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterOutlet, RouterLink, RouterLinkActive, InputTextModule, CalendarModule, FloatLabelModule, CardModule, CommonModule, TableModule, ButtonModule],
+  imports: [RouterOutlet, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Angular-Appointment-Application';
-  appointments: Appointment[] = [
-    new Appointment("Erkam Yaman", new Date(), "Hair"),
-    new Appointment("Erkam Yaman", new Date(), "Nail"),
-    new Appointment("Erkam Yaman", new Date(), "Shave")
-  ]
-  handleClick() {
-    console.log('clicked')
-  }
 
-  dateTime: Date;
-
-  constructor() {
-    this.dateTime = new Date();
-  }
 }
