0f5f97d9b94bc00093e169825f67798bff135c6b
Kam <erkamyaman35@gmail.com>
2024-07-30 17:40:29 +0300

    form-component-start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,15 @@ import { Appointment } from '../shared/models/appoinment';
 import { ButtonModule } from 'primeng/button';
 import { CommonModule } from '@angular/common';
 import { TableModule } from 'primeng/table';
+import { InputTextModule } from 'primeng/inputtext';
+import { CardModule } from 'primeng/card';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { CalendarModule } from 'primeng/calendar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, TableModule, ButtonModule],
+  imports: [RouterOutlet, InputTextModule, CalendarModule, FloatLabelModule, CardModule, CommonModule, TableModule, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -21,5 +25,11 @@ export class AppComponent {
   ]
   handleClick() {
     console.log('clicked')
+  }
+
+  dateTime: Date;
+
+  constructor() {
+    this.dateTime = new Date();
   }
 }
---
+++ src/app/my-form/my-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-form.component.html',
+  styleUrl: './my-form.component.css'
+})
+export class MyFormComponent {
+
+}
